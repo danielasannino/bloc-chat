e6b827ec7d53bb30e2ea1f5670d6dacbe6b9f057
Daniela Sannino <danielasannino@Danielas-MacBook-Pro.local>
2017-06-13 16:29:15 +0200

    created Message.js

---
+++ app/scripts/services/Message.js
@@ -0,0 +1,13 @@
+(function() {
+    function Message($firebaseArray) {
+        var Message = {};
+        var ref = firebase.database().ref().child("messages");
+        
+        return Message;
+    }
+    
+    angular
+        .module('blocChat')
+        .factory('Message', ['$firebaseArray', Message]);
+        
+})();
