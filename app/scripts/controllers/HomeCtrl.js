e682c0d462d2aa19e64bd80d0a720f0dc070710f
Daniela Sannino <danielasannino@Danielas-MacBook-Pro.local>
2017-06-13 11:16:49 +0200

    create chat rooms

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -2,12 +2,12 @@
     function HomeCtrl(Room, $uibModal) { 
         this.rooms = Room.all;
         this.addRoom = function() {
- +            $uibModal.open({
- +                templateUrl: '/templates/modal.html',
- +                size: 'sm',
- +                controller: 'ModalCtrl as modal'
- +            });
- +        }
+             $uibModal.open({
+                 templateUrl: '/templates/modal.html',
+                 size: 'sm',
+                 controller: 'ModalCtrl as modal'
+             });
+         }
     }
     
     angular
