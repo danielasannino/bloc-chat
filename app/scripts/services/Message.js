8b55f23e45446b8b197885c368e2b92e485600a6
Daniela Sannino <danielasannino@Danielas-MacBook-Pro.local>
2017-06-14 15:00:00 +0200

    checkpoint 6

--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -1,11 +1,18 @@
 (function() {
     function Message($firebaseArray) {
         var Message = {};
+        
         var ref = firebase.database().ref().child("messages");
         var messages = $firebaseArray(ref);
+        Message.all = messages;
         
-        Message.getByRoomId = function(roomId) {
+        Message.getByRoomId = function (roomId) {
             return $firebaseArray(ref.orderByChild('roomId').equalTo(roomId));
+        }
+        
+        Message.send = function (newMessage) {
+            messages.$add(newMessage);
+            newMessage.sentAt = firebase.database.ServerValue.TIMESTAMP;
         }
         
         return Message;
@@ -14,5 +21,4 @@
     angular
         .module('blocChat')
         .factory('Message', ['$firebaseArray', Message]);
-        
 })();
--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -1,19 +1,20 @@
 (function() {
-  function Room($firebaseArray) {
-    var Room = {};
-    var ref = firebase.database().ref().child("rooms");
-    var rooms = $firebaseArray(ref);
-
-    Room.all = rooms;
-      
-    Room.add = function(room) {
-        rooms.$add(room);
+    function Room($firebaseArray) {
+        var Room = {};
+        
+        var ref = firebase.database().ref().child("rooms");
+        var rooms = $firebaseArray(ref);
+        
+        Room.all = rooms;
+        
+        Room.add = function (room) {
+            rooms.$add(room);
+        };
+        
+        return Room;
     }
-
-    return Room;
-  }
-
-  angular
-    .module('blocChat')
-    .factory('Room', ['$firebaseArray', Room]);
+    
+    angular
+        .module('blocChat')
+        .factory('Room', ['$firebaseArray', Room]);
 })();
